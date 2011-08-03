3ab63f5b9a26c66150f91d5b8937b45ed8791813
Woody Gilk <woody@wingsc.com>
2011-08-02 21:23:47 -0500

    curl 7.21.7
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Curl < Formula
   homepage 'http://curl.haxx.se/'
-  url 'http://curl.haxx.se/download/curl-7.21.6.tar.bz2'
-  sha256 'd9a3d3593796147ad9ca994c9e6834a42b49756420a10e996dbf849495d3d955'
+  url 'http://curl.haxx.se/download/curl-7.21.7.tar.bz2'
+  sha256 '1a50dd17400c042090203eef347e946f29c342c32b6c4843c740c80975e8215a'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
