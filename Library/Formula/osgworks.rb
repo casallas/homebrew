76bb9d4b1536530c6dded87f57b8bc51dd3b1716
Juan Sebastian Casallas <casallas@iastate.edu>
2011-10-13 00:16:40 +0200

    osgWorks: Added url to download source tarball, solved audit problems.
    -Added url field to download the source tarball.
    -Added sha1 checksum as provided by google code.
    -Changed head to download from the svn trunk and not from the 1_01-branch.

--- Library/Formula/osgworks.rb
+++ Library/Formula/osgworks.rb
@@ -1,8 +1,10 @@
 require 'formula'
 
-class Osgworks <Formula
-  head 'http://osgworks.googlecode.com/svn/branches/1_01-branch', :using => :svn
-  homepage 'http://code.google.com/p/osgworks'
+class Osgworks < Formula
+  url 'http://osgworks.googlecode.com/files/osgWorks_01_01_00.tar.gz'
+  sha1 '1b50d29b5dcad6335c16d0db28f1bfaef07fcf74'
+  head 'http://osgworks.googlecode.com/svn/trunk/'
+  homepage 'http://code.google.com/p/osgworks/'
   version '1.1'
 
   depends_on 'cmake'
