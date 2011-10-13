3e60dd3c44766d2a2e0dafc5b3e57d6402fa16e9
Juan Sebastian Casallas <casallas@iastate.edu>
2011-10-13 02:31:46 +0200

    osgWorks: Apply a patch when not installing from HEAD
    -OpenSceneGraph changed DisplaySettings::instance's return type.
    -CameraConfigObject::store uses this method so we need to patch it.
    -osgWorks svn head or 2.0 RC1 doesn't need the patch.

--- Library/Formula/osgworks.rb
+++ Library/Formula/osgworks.rb
@@ -14,4 +14,27 @@ class Osgworks < Formula
     system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
+
+  def patches
+    # openscenegraph 3 changed DisplaySettings::instance, thus we need to patch CameraConfigObject.cpp
+    # This should be solved in osgWorks' HEAD (2.0 RC1)
+    return DATA unless ARGV.build_head?
+  end
+ 
 end
+
+__END__
+diff --git a/src/osgwTools/CameraConfigObject.cpp b/src/osgwTools/CameraConfigObject.cpp
+index 76d767c..eac0e4b 100644
+--- a/src/osgwTools/CameraConfigObject.cpp
++++ b/Users/juan/Desktop/CameraConfigObject.cpp
+@@ -101,7 +101,7 @@ CameraConfigObject::store( osgViewer::Viewer& viewer )
+     }
+ 
+     osg::DisplaySettings* ds = masterCamera->getDisplaySettings() != NULL ?
+-        masterCamera->getDisplaySettings() : osg::DisplaySettings::instance();
++        masterCamera->getDisplaySettings() : osg::DisplaySettings::instance().get();
+     
+     double fovy, aspectRatio, zNear, zFar;        
+     masterCamera->getProjectionMatrixAsPerspective(fovy, aspectRatio, zNear, zFar);
+
