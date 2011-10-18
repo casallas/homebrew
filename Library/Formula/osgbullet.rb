1830edbd6ccb477535e0e30c79f02c346b4254fc
Juan Sebastian Casallas <casallas@iastate.edu>
2011-10-18 12:29:53 +0200

    osgBullet: Update to 2.0RC3
    -Added url and sha1 to download the 2.0RC3 tarball.
    -osgBullet's trunk is now the formula's head.
    -Added a version number so that brew doesn't get confused by the tarball name.
    -Added a blank space brefore "Formula" to pass brew audit.

--- Library/Formula/osgbullet.rb
+++ Library/Formula/osgbullet.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
-class Osgbullet <Formula
-  head 'http://osgbullet.googlecode.com/svn/branches/1_01-branch'
+class Osgbullet < Formula
+  url 'http://osgbullet.googlecode.com/files/osgBullet_02_00_00-rc3.zip'
+  sha1 '92f8035d50e90072ddc407dce589c131c2440928' 
+  head 'http://osgbullet.googlecode.com/svn/trunk/'
   homepage 'http://code.google.com/p/osgbullet/'
-
+  version '2.0RC3'
 
   depends_on 'cmake'
   depends_on 'openscenegraph'
