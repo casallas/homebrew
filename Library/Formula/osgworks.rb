c7c1f7891cf1a7a6f88226d674bc47923dba2671
Juan Sebastian Casallas <casallas@iastate.edu>
2011-11-07 00:08:26 +0100

    osgWorks: Update 2.0
    -Previous version was 2.0RC1
    -Updated url, sha1 and version for the 2.0 tarball

--- Library/Formula/osgworks.rb
+++ Library/Formula/osgworks.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Osgworks < Formula
-  url 'http://osgworks.googlecode.com/files/osgWorks_02_00_00-rc1.zip'
-  sha1 '04c2dcc60bdd0a11c55545c686e868a2f43b910c'
+  url 'http://osgworks.googlecode.com/files/osgWorks_02_00_00.tar.gz'
+  sha1 '109c4b253f26345c51e0f5df0b3edaea3732485f'
   head 'http://osgworks.googlecode.com/svn/trunk/'
   homepage 'http://code.google.com/p/osgworks/'
-  version '2.0RC1'
+  version '2.0'
 
   depends_on 'cmake'
   depends_on 'openscenegraph'
@@ -15,26 +15,4 @@ class Osgworks < Formula
     system "make install"
   end
 
-  #def patches
-    # openscenegraph 3 changed DisplaySettings::instance, thus we need to patch CameraConfigObject.cpp
-    # This should be solved in osgWorks' HEAD (2.0 RC1)
-    #return DATA unless ARGV.build_head?
-  #end
- 
 end
-
-__END__
-diff --git a/src/osgwTools/CameraConfigObject.cpp b/src/osgwTools/CameraConfigObject.cpp
-index 76d767c..eac0e4b 100644
---- a/src/osgwTools/CameraConfigObject.cpp
-+++ b/Users/juan/Desktop/CameraConfigObject.cpp
-@@ -101,7 +101,7 @@ CameraConfigObject::store( osgViewer::Viewer& viewer )
-     }
- 
-     osg::DisplaySettings* ds = masterCamera->getDisplaySettings() != NULL ?
--        masterCamera->getDisplaySettings() : osg::DisplaySettings::instance();
-+        masterCamera->getDisplaySettings() : osg::DisplaySettings::instance().get();
-     
-     double fovy, aspectRatio, zNear, zFar;        
-     masterCamera->getProjectionMatrixAsPerspective(fovy, aspectRatio, zNear, zFar);
-
