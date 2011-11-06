22ff1da514ab6628c75ffa041034b2a5f6eaba62
Juan Sebastian Casallas <casallas@iastate.edu>
2011-11-07 00:06:30 +0100

    osgBullet: Update to 2.0
    -Previous version was 2.0RC3
    -Updated url and sha1 to download the 2.0 tarball

--- Library/Formula/osgbullet.rb
+++ Library/Formula/osgbullet.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Osgbullet < Formula
-  url 'http://osgbullet.googlecode.com/files/osgBullet_02_00_00-rc3.zip'
-  sha1 '92f8035d50e90072ddc407dce589c131c2440928' 
+  url 'http://osgbullet.googlecode.com/files/osgBullet_02_00_00.tar.gz'
+  sha1 '7715f989307667598612dd7b862cba31bf64a003' 
   head 'http://osgbullet.googlecode.com/svn/trunk/'
   homepage 'http://code.google.com/p/osgbullet/'
-  version '2.0RC3'
+  version '2.0'
 
   depends_on 'cmake'
   depends_on 'openscenegraph'
