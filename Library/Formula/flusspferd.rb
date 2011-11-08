d9fa8a51503556e762f0ebec74bb186367d1f727
Juan Sebastian Casallas <casallas@iastate.edu>
2011-11-08 10:23:21 +0100

    flusspferd: Updated dead url, added support for SpiderMonkey 1.8.5
    -flusspferd.org seems to be down so tarball is now downloaded from github
    -Updated md5 for the new tarball
    -Added a patch for the FindSpiderMonkey.cmake module to support SM 1.8.5

--- Library/Formula/flusspferd.rb
+++ Library/Formula/flusspferd.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Flusspferd < Formula
-  url 'http://flusspferd.org/downloads/flusspferd-0.9.tar.bz2'
+  url 'https://github.com/Flusspferd/flusspferd/tarball/v0.9'
   homepage 'http://flusspferd.github.com/'
-  md5 '7688b2a939777b4b7be82898dea9b3d9'
+  md5 '8d839b9ab25e1f3fcb9c0e48fb3538b9'
 
   depends_on 'cmake' => :build
   depends_on 'arabica'
@@ -12,6 +12,11 @@ class Flusspferd < Formula
   depends_on 'spidermonkey'
 
   fails_with_llvm
+
+  #Fix CMakeModules/FindSpidermonkey.cmake to be able to find SM 1.8.5
+  def patches
+    "https://raw.github.com/gist/1347323/flusspferd-cmake_find_spidermonkey1.8.5.patch"
+  end
 
   def install
     system "cmake -H. -Bbuild #{std_cmake_parameters}"
