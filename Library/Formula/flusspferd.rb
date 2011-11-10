6c7b285ad9bef24b5a3fd3cb7c44f881df89c81a
Juan Sebastian Casallas <casallas@iastate.edu>
2011-11-10 08:55:29 +0100

    Flusspferd: Changed url to git repository
    -flusspferd uses git submodules when building
    -When downlading the tag tarball, the folder is not a git repo
    -Downloading the git repo makes sure submodules work
    -Added a patch to download submodules via https in case user is behind a firewall

--- Library/Formula/flusspferd.rb
+++ Library/Formula/flusspferd.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Flusspferd < Formula
-  url 'https://github.com/Flusspferd/flusspferd/tarball/v0.9'
+  url 'https://github.com/Flusspferd/flusspferd.git'
   homepage 'http://flusspferd.github.com/'
-  md5 '8d839b9ab25e1f3fcb9c0e48fb3538b9'
+  version '0.9'
 
   depends_on 'cmake' => :build
   depends_on 'arabica'
@@ -16,10 +16,12 @@ class Flusspferd < Formula
   #Fix CMakeModules/FindSpidermonkey.cmake to be able to find SM 1.8.5
   def patches
     "https://raw.github.com/gist/1347323/flusspferd-cmake_find_spidermonkey1.8.5.patch"
+    "https://raw.github.com/gist/49c4e09a8285d4d2c4d0/flusspferd-git_to_https_submodules.patch"
   end
 
   def install
     system "cmake -H. -Bbuild #{std_cmake_parameters}"
+    system "git submodule sync"
     system "make install"
   end
 end
