f2a5dd8f6d507c382f739ec3a75beb677f4ec032
Juan Sebastian Casallas <casallas@iastate.edu>
2011-11-21 10:23:54 +0100

    Split blahtexml into blahtex and blahtexml
    -Some libraries only need blahtex like Instill or Maruku
    -It isn't worth building xerces-c (needed for blahtexml only) when you only need blahtex
    -No current formula depends on blahtexml so this change won't affect other formulae

--- Library/Formula/blahtex.rb
+++ Library/Formula/blahtex.rb
@@ -1,21 +1,19 @@
 require 'formula'
 
-class Blahtexml < Formula
+class Blahtex < Formula
   url 'http://gva.noekeon.org/blahtexml/blahtexml-0.9-src.tar.gz'
   homepage 'http://gva.noekeon.org/blahtexml/'
   md5 'ed790599223c2f8f6d205be8988882de'
 
-  depends_on 'xerces-c'
-
   def install
-    system "make blahtexml-mac"
-    bin.install('blahtexml')
+    system "make blahtex-mac"
+    bin.install('blahtex')
   end
 
   def caveats; <<-EOS.undent
-    This formula only installs blahtexml, not blahtex.
-    If you want to install blahtex, then do:
-      brew install blahtex
+    This formula only installs blahtex, not blahtexml.
+    If you want to install blahtexml, then do:
+      brew install blahtexml
     EOS
   end
 end
