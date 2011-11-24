19c2dba4b23a6a9bd35b61badd89212416744ac9
Juan Sebastian Casallas <casallas@iastate.edu>
2011-11-24 13:38:22 +0100

    Add --only-blahtex option to blahtexml

--- Library/Formula/blahtexml.rb
+++ Library/Formula/blahtexml.rb
@@ -5,17 +5,32 @@ class Blahtexml < Formula
   homepage 'http://gva.noekeon.org/blahtexml/'
   md5 'ed790599223c2f8f6d205be8988882de'
 
-  depends_on 'xerces-c'
+  #depends_on 'xerces-c' unless ARGV.include? '--blahtex-only'
+
+  def options
+    [
+      ['--blahtex-only', "Build only blahtex, not blahtexml"]
+    ]
+  end
+
 
   def install
-    system "make blahtexml-mac"
-    bin.install('blahtexml')
+    if ARGV.include? '--blahtex-only'
+      system "echo blahtex-only"
+    end
+
+    system "make blahtex-mac"
+    bin.install('blahtex')
+    unless ARGV.include? '--blahtex-only'
+      system "make blahtexml-mac"
+      bin.install('blahtexml')
+    end
   end
 
   def caveats; <<-EOS.undent
-    This formula only installs blahtexml, not blahtex.
-    If you want to install blahtex, then do:
-      brew install blahtex
+    This formula installs both blahtex and blahtexml.
+    If you only want to install blahtex, then do:
+      brew install blahtexml --blahtex-only
     EOS
   end
 end
