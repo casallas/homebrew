efe6f7e98906ed7dec7a4ca4f13c05b0440c3215
Juan Sebastian Casallas <casallas@iastate.edu>
2012-01-22 02:33:36 -0600

    Move --env from bin/brew to its own file
    -Also add dump_build_env to (new) compatibility.rb
    -Part of 9afc85ad338f9e053fe36127937d308ad62593bc

--- Library/Homebrew/compatibility.rb
+++ Library/Homebrew/compatibility.rb
@@ -3,3 +3,8 @@ def dump_config
   require 'cmd/--config'
   Homebrew.__config
 end
+
+def dump_build_env env
+  require 'cmd/--env'
+  Homebrew.dump_build_env env
+end
--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -266,28 +266,6 @@ def nostdout
   end
 end
 
-def dump_build_env env
-  puts "\"--use-llvm\" was specified" if ARGV.include? '--use-llvm'
-
-  %w[ CC CXX LD ].each do |k|
-    value = env[k]
-    if value
-      results = value
-      if File.exists? value and File.symlink? value
-        target = Pathname.new(value)
-        results += " => #{target.realpath}"
-      end
-      puts "#{k}: #{results}"
-    end
-  end
-
-  %w[ CFLAGS CXXFLAGS CPPFLAGS LDFLAGS MACOSX_DEPLOYMENT_TARGET MAKEFLAGS PKG_CONFIG_PATH
-      HOMEBREW_DEBUG HOMEBREW_VERBOSE HOMEBREW_USE_LLVM HOMEBREW_SVN ].each do |k|
-    value = env[k]
-    puts "#{k}: #{value}" if value
-  end
-end
-
 def x11_installed?
   File.exist?(SystemCommand.which 'X')
 end
