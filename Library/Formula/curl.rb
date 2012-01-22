22662c87b0bd15b8eb23bd955a20540e810fedb6
Juan Sebastian Casallas <casallas@iastate.edu>
2012-01-22 04:17:29 -0600

    Re-add curl, remove "keg_only"
    -Some linux distributions might benefit from a newer version of curl

---
+++ Library/Formula/curl.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Curl < Formula
+  homepage 'http://curl.haxx.se/'
+  url 'http://curl.haxx.se/download/curl-7.23.1.tar.bz2'
+  sha256 '7d8c8cef8f244118245c37853cb175bd95c156ff453730ec2c88189b05a7c06e'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
