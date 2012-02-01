8d93bcabeb9e6f5e32722725306502650dc8f97a
Juan Sebastian Casallas <casallas@iastate.edu>
2012-02-01 17:32:15 -0600

    Move macports_or_fink_installed to MacOS module in utils
    -Also add to compatibility when not on linux
    -Part of 9afc85ad338f9e053fe36127937d308ad62593bc

--- Library/Homebrew/compatibility.rb
+++ Library/Homebrew/compatibility.rb
@@ -8,3 +8,11 @@ def dump_build_env env
   require 'cmd/--env'
   Homebrew.dump_build_env env
 end
+
+def macports_or_fink_installed?
+  if SystemCommand.platform == :linux
+    return false
+  else
+    MacOS.macports_or_fink_installed?
+  end
+end
--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -269,3 +269,36 @@ end
 def x11_installed?
   File.exist?(SystemCommand.which 'X')
 end
+
+module MacOS extend self
+  def macports_or_fink_installed?
+    # See these issues for some history:
+    # http://github.com/mxcl/homebrew/issues/#issue/13
+    # http://github.com/mxcl/homebrew/issues/#issue/41
+    # http://github.com/mxcl/homebrew/issues/#issue/48
+
+    %w[port fink].each do |ponk|
+      path = `/usr/bin/which -s #{ponk}`
+      return ponk unless path.empty?
+    end
+
+    # we do the above check because macports can be relocated and fink may be
+    # able to be relocated in the future. This following check is because if
+    # fink and macports are not in the PATH but are still installed it can
+    # *still* break the build -- because some build scripts hardcode these paths:
+    %w[/sw/bin/fink /opt/local/bin/port].each do |ponk|
+      return ponk if File.exist? ponk
+    end
+
+    # finally, sometimes people make their MacPorts or Fink read-only so they
+    # can quickly test Homebrew out, but still in theory obey the README's
+    # advise to rename the root directory. This doesn't work, many build scripts
+    # error out when they try to read from these now unreadable directories.
+    %w[/sw /opt/local].each do |path|
+      path = Pathname.new(path)
+      return path if path.exist? and not path.readable?
+    end
+
+    false
+  end
+end
