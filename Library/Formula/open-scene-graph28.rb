45e4ab7dc41dc0692b86e477d55fc8f4310f3066
Juan Sebastian Casallas <casallas@iastate.edu>
2012-04-13 13:08:24 -0500

    osg28: Changes to work in linux
    -Comment out dependencies that aren't yet available
    -Get rid of mac specifics
    -Rename lib64 to lib, lib64 isn't supported by homebrew

--- Library/Formula/open-scene-graph28.rb
+++ Library/Formula/open-scene-graph28.rb
@@ -11,23 +11,16 @@ class OpenSceneGraph28 < Formula
   depends_on 'cmake' => :build
   depends_on 'jpeg'
   depends_on 'wget'
-  depends_on 'ffmpeg' => :optional
-  depends_on 'gdal' => :optional
-  depends_on 'jasper' => :optional
-  depends_on 'openexr' => :optional
-  depends_on 'collada' => :optional
-  depends_on 'dcmtk' => :optional
-  depends_on 'librsvg' => :optional
+  #depends_on 'ffmpeg' => :optional
+  #depends_on 'gdal' => :optional
+  #depends_on 'jasper' => :optional
+  #depends_on 'openexr' => :optional
+  #depends_on 'collada' => :optional
+  #depends_on 'dcmtk' => :optional
+  #depends_on 'librsvg' => :optional
 
   def install
     args = ["..", "-DCMAKE_INSTALL_PREFIX='#{prefix}'", "-DCMAKE_BUILD_TYPE=None", "-Wno-dev", "-DBUILD_OSG_WRAPPERS=ON", "-DBUILD_DOCUMENTATION=ON"]
-    if snow_leopard_64?
-      args << "-DCMAKE_OSX_ARCHITECTURES=x86_64"
-      args << "-DOSG_DEFAULT_IMAGE_PLUGIN_FOR_OSX=imageio"
-      args << "-DOSG_WINDOWING_SYSTEM=Cocoa"
-    else
-      args << "-DCMAKE_OSX_ARCHITECTURES=i386"
-    end
 
     if Formula.factory('collada').installed?
       args << "-DCOLLADA_INCLUDE_DIR=#{HOMEBREW_PREFIX}/include/collada-dom"
@@ -38,6 +31,8 @@ class OpenSceneGraph28 < Formula
       system "cmake", *args
       system "make install"
     end
+    # Libs are installed to /lib64 but we don't have that in homebrew, rename to /lib
+    File.rename "#{prefix}/lib64" "#{lib}"
   end
 
   def patches
