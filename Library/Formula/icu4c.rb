037862c63243facd1a3983d0bb1993ec17482dbd
Juan Sebastian Casallas <casallas@iastate.edu>
2012-09-26 20:34:44 -0500

    Re-add icu4c
    -Taken from c25fd2fc11b2b4b9589d3463fc3d3040f916bdaf
    -Mac specifics and not-yet-implemented features removed

---
+++ Library/Formula/icu4c.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Icu4c < Formula
+  homepage 'http://site.icu-project.org/'
+  url 'http://download.icu-project.org/files/icu4c/49.1.2/icu4c-49_1_2-src.tgz'
+  version '49.1.2'
+  sha1 'd8cee6d2b2a91a0da7464acd97a5b7b462d93225'
+
+  def install
+    ENV.append "LDFLAGS", "-headerpad_max_install_names"
+    args = ["--prefix=#{prefix}", "--disable-samples", "--disable-tests", "--enable-static"]
+    args << "--with-library-bits=64"
+    cd "source" do
+      system "./configure", *args
+      system "make"
+      system "make install"
+    end
+  end
+end
