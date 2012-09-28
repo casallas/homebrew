91a00c6f7fa2862de0cba382a3364b90dd51d89b
Juan Sebastian Casallas <casallas@iastate.edu>
2012-09-28 16:18:12 -0500

    Move check_for_blacklist.. and brew_install to cmd/install
    -Use linux SystemCommand from 1be1867645653a03b55c3f4319d598ed4258eba4
    Part of 9afc85ad338f9e053fe36127937d308ad62593bc

--- Library/Homebrew/brew.h.rb
+++ Library/Homebrew/brew.h.rb
@@ -1,42 +1,3 @@
-def check_for_blacklisted_formula names
-  return if ARGV.force?
-
-  names.each do |name|
-    case name
-    when 'tex', 'tex-live', 'texlive' then abort <<-EOS.undent
-      Installing TeX from source is weird and gross, requires a lot of patches,
-      and only builds 32-bit (and thus can't use Homebrew deps on Snow Leopard.)
-
-      We recommend using a MacTeX distribution:
-        http://www.tug.org/mactex/
-    EOS
-
-    when 'mercurial', 'hg' then abort <<-EOS.undent
-      Mercurial can be install thusly:
-        brew install pip && pip install mercurial
-    EOS
-
-    when 'npm' then abort <<-EOS.undent
-      npm can be installed thusly by following the instructions at
-        http://npmjs.org/
-
-      To do it in one line, use this command:
-        curl http://npmjs.org/install.sh | sh
-    EOS
-
-
-    when 'setuptools' then abort <<-EOS.undent
-      When working with a Homebrew-built Python, distribute is preferred
-      over setuptools, and can be used as the prerequisite for pip.
-
-      Install distribute using:
-        brew install distribute
-    EOS
-    end
-  end
-end
-
-
 def editmake url
   path = Pathname.new url
 
@@ -66,53 +27,6 @@ def clean f
     if d.children.empty? and not f.skip_clean? d
       puts "rmdir: #{d} (empty)" if ARGV.verbose?
       d.rmdir
-    end
-  end
-end
-
-def brew_install
-  require 'formula_installer'
-  require 'hardware'
-
-  ############################################################ sanity checks
-  case Hardware.cpu_type when :ppc, :dunno
-    abort "Sorry, Homebrew does not support your computer's CPU architecture.\n"+
-          "For PPC support, see: http://github.com/sceaga/homebrew/tree/powerpc"
-  end
-
-  raise "Cannot write to #{HOMEBREW_CELLAR}" if HOMEBREW_CELLAR.exist? and not HOMEBREW_CELLAR.writable?
-  raise "Cannot write to #{HOMEBREW_PREFIX}" unless HOMEBREW_PREFIX.writable?
-
-  ################################################################# warnings
-  begin
-    if SystemCommand.platform == :mac
-      if MACOS_VERSION >= 10.6
-        opoo "You should upgrade to Xcode 3.2.3" if llvm_build < RECOMMENDED_LLVM
-      else
-        opoo "You should upgrade to Xcode 3.1.4" if (gcc_40_build < RECOMMENDED_GCC_40) or (gcc_42_build < RECOMMENDED_GCC_42)
-      end
-    end
-  rescue
-    # the reason we don't abort is some formula don't require Xcode
-    # TODO allow formula to declare themselves as "not needing Xcode"
-    opoo "Xcode is not installed! Builds may fail!"
-  end
-
-  if macports_or_fink_installed?
-    opoo "It appears you have MacPorts or Fink installed."
-    puts "Software installed with MacPorts and Fink are known to cause problems."
-    puts "If you experience issues try uninstalling these tools."
-  end
-
-  ################################################################# install!
-  installer = FormulaInstaller.new
-  installer.install_deps = !ARGV.include?('--ignore-dependencies')
-
-  ARGV.formulae.each do |f|
-    if not f.installed? or ARGV.force?
-      installer.install f
-    else
-      puts "Formula already installed: #{f.prefix}"
     end
   end
 end
